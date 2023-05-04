26356a9ef2c5b9d18b27bc808719b3915dbf116d
Joshua Trono <josh.trono@gmail.com>
2023-05-04 18:18:57 -0400

    added tests and had to fix blogs has_many

--- backend-ROR/test/controllers/user_controller_test.rb
+++ backend-ROR/test/controllers/user_controller_test.rb
@@ -1,13 +1,18 @@
 require "test_helper"
 
 class UserControllerTest < ActionDispatch::IntegrationTest
+  fixtures :users
+  test "should get create and return error about already been taken" do
+    post user_index_url, params: { email: users(:jt_one).email, password: "Heyther123", first_name: users(:jt_one).first_name,  last_name: users(:jt_one).first_name }
+    assert_response :bad_request
+  end
   test "should get create" do
-    get user_create_url
+    post user_index_url, params: { email: "Test1234", password: "Heytest1234", first_name: users(:jt_one).first_name,  last_name: users(:jt_one).first_name }
     assert_response :success
   end
 
-  test "should get destroy" do
-    get user_destroy_url
-    assert_response :success
-  end
+  # test "should get destroy" do
+  #   get user_destroy_url
+  #   assert_response :success
+  # end
 end
