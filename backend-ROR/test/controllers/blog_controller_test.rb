0229dd9efd450d63806352daa5da292e2a86392f
Dustyn Bachetti <dustyn821@revature.com>
2023-05-04 17:44:58 -0600

    Pushing token requirement for blog controller actions

--- backend-ROR/test/controllers/blog_controller_test.rb
+++ backend-ROR/test/controllers/blog_controller_test.rb
@@ -1,4 +1,5 @@
 require "test_helper"
+require_relative '../../lib/json_web_token'
 
 class BlogControllerTest < ActionDispatch::IntegrationTest
   test "should get index" do
@@ -13,23 +14,32 @@ class BlogControllerTest < ActionDispatch::IntegrationTest
 
   test "should create" do
     user = users(:dustynb1).id
-    # token = testUser
+    token = JsonWebToken.encode(user_id: user)
     assert_difference 'Blog.count', 1 do
       post "/user/#{user}/blog", params: { title: 'Test blog', content: 'Test blog content...',
-                                      view_count: 10, user_id: user }, headers: { Authorization: "Bearer #{}" }, as: :json
+                                      view_count: 10 }, headers: { Authorization: "Bearer #{token}" }, as: :json
       assert_response :created
     end
   end
 
   test "should not create with invalid input" do
     user = users(:dustynb1).id
-    # token = testUser
+    token = JsonWebToken.encode(user_id: user)
     assert_no_difference 'Blog.count' do
       post "/user/#{user}/blog", params: { title: '', content: 'Test blog content...',
-                                           view_count: 10, user_id: user }, headers: { Authorization: "Bearer #{}" }, as: :json
+                                           view_count: 10 }, headers: { Authorization: "Bearer #{token}" }, as: :json
       assert_response :unprocessable_entity
     end
   end
+
+  # test "should create if token invalid" do # ADD ERROR HANDLING IN JSONWebToken BEFORE UNCOMMENTING
+  #   user = users(:dustynb1).id
+  #   assert_difference 'Blog.count', 1 do
+  #     post "/user/#{user}/blog", params: { title: 'Test blog', content: 'Test blog content...',
+  #                                          view_count: 10 }, headers: { Authorization: "Bearer #{}" }, as: :json
+  #     assert_response :created
+  #   end
+  # end
 
   test "should get update" do
     get blog_update_url
