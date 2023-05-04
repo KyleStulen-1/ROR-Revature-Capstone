db66e32e959676f12b24bc668f6c25fbc81cfa95
Joshua Trono <josh.trono@gmail.com>
2023-05-04 16:37:23 -0400

    made the create user and swapped :cascade to dependent in the user model

--- backend-ROR/app/models/user.rb
+++ backend-ROR/app/models/user.rb
@@ -6,5 +6,5 @@ class User < ApplicationRecord
     validates :password, presence: true, length: { minimum: 6 }
 
     has_secure_password
-    has_many :blogs, cascade: :destroy
+    has_many :blogs, dependent: :destroy
 end
---
+++ backend-ROR/db/schema.rb
@@ -0,0 +1,52 @@
+# This file is auto-generated from the current state of the database. Instead
+# of editing this file, please use the migrations feature of Active Record to
+# incrementally modify your database, and then regenerate this schema definition.
+#
+# This file is the source Rails uses to define your schema when running `bin/rails
+# db:schema:load`. When creating a new database, `bin/rails db:schema:load` tends to
+# be faster and is potentially less error prone than running all of your
+# migrations from scratch. Old migrations may fail to apply correctly if those
+# migrations use external dependencies or application code.
+#
+# It's strongly recommended that you check this file into your version control system.
+
+ActiveRecord::Schema[7.0].define(version: 2023_05_04_184906) do
+  create_table "blogs", force: :cascade do |t|
+    t.string "title"
+    t.string "content"
+    t.integer "view_count"
+    t.integer "user_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_blogs_on_user_id"
+  end
+
+  create_table "reactions", force: :cascade do |t|
+    t.integer "user_id", null: false
+    t.integer "blogs_id", null: false
+    t.integer "react_thumb"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["blogs_id"], name: "index_reactions_on_blogs_id"
+    t.index ["user_id"], name: "index_reactions_on_user_id"
+  end
+
+  create_table "topics", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "email"
+    t.string "password_digest"
+    t.string "first_name"
+    t.string "last_name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  add_foreign_key "blogs", "users"
+  add_foreign_key "reactions", "blogs", column: "blogs_id"
+  add_foreign_key "reactions", "users"
+end
