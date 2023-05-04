5fe30bb0bf4f2d959bb452fba081c433080849c1
Xien Thomas <xien.thomas@gmail.com>
2023-05-04 15:01:10 -0500

    adding controllers

--- backend-ROR/config/routes.rb
+++ backend-ROR/config/routes.rb
@@ -1,4 +1,11 @@
 Rails.application.routes.draw do
+  get 'blog/index'
+  get 'blog/show'
+  get 'blog/create'
+  get 'blog/update'
+  get 'blog/destroy'
+  get 'user/create'
+  get 'user/destroy'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
