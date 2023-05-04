8051ac6eb34f18d63b58546ee28e3db3d2e944cc
Xien Thomas <xien.thomas@gmail.com>
2023-05-04 17:07:42 -0500

    [wip] updating tests to update controller

--- backend-ROR/app/models/user.rb
+++ backend-ROR/app/models/user.rb
@@ -7,4 +7,5 @@ class User < ApplicationRecord
 
     has_secure_password
     has_many :blogs, dependent: :destroy
+
 end
--- backend-ROR/test/controllers/blog_controller_test.rb
+++ backend-ROR/test/controllers/blog_controller_test.rb
@@ -2,8 +2,15 @@ require "test_helper"
 
 class BlogControllerTest < ActionDispatch::IntegrationTest
 
+  def setup
+    @userxt = users(:xtone)
+    @userxt_token = JsonWebToken.encode('user_id': @userxt.id)
+    @Auth_userxt = {'Authorization' => "Bearer #{@userxt_token}"}
+    @blogxt = blog(:xtone)
+  end 
+
   test "user should be able to update blog" do
-    get blog_update_url
+    put user_blog_url(@userxt, @blogxt), header: @Auth_userxt, params: {title: 'change title', content: 'Auth_userxt content 123'} ,as: :json
     assert_response :success
   end
 
