1db9ef79a8cfe3f30a4c31cedab3e39fe7e5bcf4
Kyle Stulen <62697063+KyleStulen-1@users.noreply.github.com>
2023-05-08 17:52:40 -0500

    Update App.tsx

--- frontend-react/src/App.tsx
+++ frontend-react/src/App.tsx
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import React, {useState} from 'react';
 import {BrowserRouter, Route, Routes} from "react-router-dom";
 
@@ -23,7 +22,7 @@ function App() {
           <Route path="/blogs" element={<Blogs />} />
           <Route path="/newblog" element={<NewBlog />} />
           <Route path="/editblogs/:id" element={<EditBlogs />} />
-          <Route path="/myblogs" element={<MyBlogs />} />
+          <Route path="/myblogs" element={<MyBlogs currentUser={principal} } />
           <Route path="/newuser" element={<NewUser currentUser={principal}/>} />
           <Route path="/login" element={<Login currentUser={principal} setCurrentUser={setPrincipal}/>} />
         </Routes>
@@ -32,38 +31,3 @@ function App() {
 }
 
 export default App;
-=======
-import React, {useState} from 'react';
-import {BrowserRouter, Route, Routes} from "react-router-dom";
-
-import './App.css';
-import Nav from "./components/Nav";
-import Blogs from "./components/Blogs"
-import NewBlog from "./components/NewBlog"
-import MyBlogs from "./components/MyBlogs"
-import NewUser from "./components/NewUser"
-import Login from "./components/Login";
-import EditBlogs from "./components/EditBlogs";
-import {User} from "./models/user"
-
-function App() {
-
-  const [principal, setPrincipal] = useState<User>();
-
-  return (
-      <BrowserRouter>
-        <Nav currentUser = {principal} setCurrentUser={setPrincipal} />
-        <Routes>
-          <Route path="/blogs" element={<Blogs />} />
-          <Route path="/newblog" element={<NewBlog />} />
-          <Route path="/editblogs/:id" element={<EditBlogs currentUser={principal}/>} />
-          <Route path="/myblogs" element={<MyBlogs currentUser={principal} />} />
-          <Route path="/newuser" element={<NewUser currentUser={principal}/>} />
-          <Route path="/login" element={<Login currentUser={principal} setCurrentUser={setPrincipal}/>} />
-        </Routes>
-      </BrowserRouter>
-  );
-}
-
-export default App;
->>>>>>> 743bf0350aea38cba43c78ba5dbcb193a6dab212
