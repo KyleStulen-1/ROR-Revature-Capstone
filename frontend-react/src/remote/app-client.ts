950a3f69c0040d099398e83e5bf927e278e4c6f0
Kyle Stulen <62697063+KyleStulen-1@users.noreply.github.com>
2023-05-08 16:39:34 -0500

    Update app-client.ts

--- frontend-react/src/remote/app-client.ts
+++ frontend-react/src/remote/app-client.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export const appClient = axios.create({
-    baseURL: 'ec2-34-229-146-172.compute-1.amazonaws.com',
+    baseURL: 'http://ec2-34-229-146-172.compute-1.amazonaws.com',
     headers: {
         'Content-Type': 'application/json'
     }
